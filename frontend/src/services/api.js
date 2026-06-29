ba96dec96d2e720feb4426f476fe5c2d99282605
Rohit315242 <gavalirohit71@gmail.com>
2026-06-29 14:06:58 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://homebite-backend-lnkr.onrender.com/api",
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  
   if (
     token &&
     !config.url.includes("/login") &&
